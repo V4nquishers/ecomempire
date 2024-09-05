801d32dd08f28fccc4acd8b9a3e13c68f2ae29ba
aicorr <aidencorreya@outlook.com>
2024-09-05 20:28:55 +0530

    fix

--- db_connect.php
+++ db_connect.php
@@ -1,17 +1,3 @@
-<!-- <?php
-
-$host = 'rdbms-mysql'; // Replace with your database host
-$dbname = 'ecomempire'; // Replace with your database name
-$username = 'root'; // Replace with your database username
-$password = '123aaron'; // Replace with your database password
-
-try {
-    $dsn = "mysql:host=$host;dbname=$dbname";
-    $conn = new PDO($dsn, $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-} -->
 <?php
 
 $host = 'velum.cinis.cf'; // Database host
