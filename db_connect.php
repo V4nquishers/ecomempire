8f0ac46b433568a76e19bd90fab9f2d51dc8be5c
aicorr <aidencorreya@outlook.com>
2024-09-05 20:27:43 +0530

    modify database connection to connect remotely

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
 $host = 'rdbms-mysql'; // Replace with your database host
 $dbname = 'ecomempire'; // Replace with your database name
@@ -7,6 +7,21 @@ $password = '123aaron'; // Replace with your database password
 
 try {
     $dsn = "mysql:host=$host;dbname=$dbname";
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+} -->
+<?php
+
+$host = 'velum.cinis.cf'; // Database host
+$port = '3308'; // Database port
+$dbname = 'ecomempire'; // Database name
+$username = 'root'; // Database username
+$password = '123aaron'; // Database password
+
+try {
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname";
     $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
